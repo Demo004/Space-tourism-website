dfe841ed89f4eb1d3145cba7a3f6f93f01464299
Ademola Taiwo <taiwoademola465@gmaile.com>
2024-06-09 14:16:31 +0100

    added other destination links

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import "../style/style.css";
 // components
 import "../style/components/header.css";
 import "../style/components/hero.css";
+import "../style/components/destination.css";
 
 // utility
 import "../style/utility.css";
@@ -26,4 +27,23 @@ document.getElementById('close-icon').addEventListener('click', function() {
 document.getElementById('overlay').addEventListener('click', function() {
     document.getElementById('sidebar').classList.remove('show-sidebar');
     document.getElementById('overlay').classList.remove('show-overlay');
+});
+
+
+
+
+
+// 
+document.querySelectorAll('.links a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop - 50, // Adjust for fixed header
+                behavior: 'smooth'
+            });
+        }
+    });
 });
